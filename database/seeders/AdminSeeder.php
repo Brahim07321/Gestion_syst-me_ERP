fb280024a7f96c64c3b5e3cbcf69b1f255fef2e5
Brahim zarid <brahimzarit1@gmail.com>
2026-04-19 15:57:23 +0100

    fix production start command

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -10,7 +10,7 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-            ['email' => 'admin@gmail.com'],
+            ['email' => 'brahim@gmail.com'],
             [
                 'name' => 'Admin',
                 'password' => bcrypt('123456'),
